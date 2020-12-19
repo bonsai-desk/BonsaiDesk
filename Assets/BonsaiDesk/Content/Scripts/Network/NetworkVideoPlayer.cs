3ca01d5e4b29f73e0b26b6917c2d6db6ecb70d4e
Cameron Ferguson <s1458162@sms.ed.ac.uk>
2020-12-19 18:16:35 -0500

    +NetworkVideoPlayer

---
+++ Assets/BonsaiDesk/Content/Scripts/Network/NetworkVideoPlayer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Mirror;
+using TMPro;
+using UnityEngine;
+
+public class NetworkVideoPlayer : NetworkBehaviour
+{
+    public enum VideoPlayerState
+    {
+        Loading,
+        Ready
+    }
+
+    public TextMeshProUGUI textMesh;
+
+
+    [SyncVar(hook = nameof(SetState))] public VideoPlayerState state;
+    
+    public void Start()
+    {
+        SetState(state, state);
+    }
+
+    [Command(ignoreAuthority = true)]
+    public void CmdSetState(VideoPlayerState newState)
+    {
+        state = newState;
+    }
+
+    public void ClickLoading()
+    {
+        CmdSetState(VideoPlayerState.Loading);
+    }
+
+    public void ClickReady()
+    {
+        CmdSetState(VideoPlayerState.Ready);
+    }
+
+    private void SetState(VideoPlayerState oldState, VideoPlayerState newState)
+    {
+        switch (newState)
+        {
+            case VideoPlayerState.Loading:
+                textMesh.text = "LOADING";
+                break;
+
+            case VideoPlayerState.Ready:
+                textMesh.text = "READY";
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+    }
+}
