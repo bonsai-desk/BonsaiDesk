f0b0ac06166e09e15d967564805f27f0044fbb56
Callum <Calxferg@gmail.com>
2020-12-23 00:27:12 -0500

    icon morph shader

---
+++ Assets/BonsaiDesk/Content/Scripts/Other/TogglePauseMorph.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class TogglePauseMorph : MonoBehaviour
+{
+    private Material material;
+    
+    void Start()
+    {
+        var meshFilter = GetComponent<MeshFilter>();
+        material = GetComponent<MeshRenderer>().material;
+        material.SetFloat("_Lerp", 0f);
+
+        var mesh = new Mesh();
+        mesh.vertices = new[]
+        {
+            new Vector3(-0.5f, 0.5f, 0),
+            new Vector3(-0.1f, 0.5f, 0),
+            new Vector3(-0.1f, -0.5f, 0),
+            new Vector3(-0.5f, -0.5f, 0),
+            
+            new Vector3(0.1f, 0.5f, 0),
+            new Vector3(0.5f, 0.5f, 0),
+            new Vector3(0.5f, -0.5f, 0),
+            new Vector3(0.1f, -0.5f, 0)
+        };
+        mesh.tangents = new[]
+        {
+            new Vector4(-0.5f, 0.5f, 0, 1),
+            new Vector4(0, 0.25f, 0, 1),
+            new Vector4(0, -0.25f, 0, 1),
+            new Vector4(-0.5f, -0.5f, 0, 1),
+            
+            new Vector4(0, 0.25f, 0, 1),
+            new Vector4(0.5f, 0, 0, 1),
+            new Vector4(0.5f, 0, 0, 1),
+            new Vector4(0, -0.25f, 0, 1)
+        };
+        mesh.triangles = new[]
+        {
+            0, 1, 2,
+            3, 0, 2,
+            
+            4, 5, 6,
+            7, 4, 6
+        };
+
+        meshFilter.sharedMesh = mesh;
+    }
+}
---
+++ Assets/BonsaiDesk/Content/Shaders/Morph.shader
@@ -0,0 +1,54 @@
+﻿Shader "Unlit/Morph"
+{
+    Properties
+    {
+        _Color("Color", Color) = (1, 1, 1, 1)
+        _Lerp("Lerp", Range(0, 1)) = 0
+    }
+    SubShader
+    {
+        Tags
+        {
+            "RenderType" = "Opaque"
+        }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            // #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float4 vertex2 : TANGENT;
+            };
+            
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+            };
+
+            float4 _Color;
+            float _Lerp;
+
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(lerp(v.vertex, v.vertex2, _Lerp));
+                return o;
+            }
+
+            fixed4 frag(v2f i) : SV_Target
+            {
+                return _Color;
+            }
+            ENDCG
+        }
+    }
+}
