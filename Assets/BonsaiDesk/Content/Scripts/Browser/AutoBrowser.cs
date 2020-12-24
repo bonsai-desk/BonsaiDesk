db5c339db19b67dbd61a5b81eef5bfec9160cc7c
Cameron Ferguson <s1458162@sms.ed.ac.uk>
2020-12-24 09:50:52 -0500

    AutoBrowser

--- Assets/BonsaiDesk/Content/Scripts/Browser/AutoBrowser.cs
+++ Assets/BonsaiDesk/Content/Scripts/Browser/AutoBrowser.cs
@@ -5,46 +5,90 @@ using Vuplex.WebView;
 
 public class AutoBrowser : MonoBehaviour
 {
+    public string initialURL;
+    public float width = 1;
+    public Vector2 aspect = new Vector2(16, 9);
+
+    public int xResolution = 850;
+    public bool autoSetResolution;
+    public float distanceEstimate = 1;
+    public int pixelPerDegree = 16;
+
+    public Texture dummyTexture;
+
     private OVROverlay _overlay;
+
+    private Vector2Int _resolution;
     private WebViewPrefab _webViewPrefab;
 
     private void Start()
     {
-        _overlay = GetComponent<OVROverlay>();
+        if (autoSetResolution)
+        {
+            var height = width * aspect.y / aspect.x;
+            var xRes = GetResolution(width, distanceEstimate, pixelPerDegree);
+            var yRes = GetResolution(height, distanceEstimate, pixelPerDegree);
+            _resolution = new Vector2Int(xRes, yRes);
+        }
+        else
+        {
+            _resolution = new Vector2Int(xResolution, (int) Math.Round(xResolution * aspect.y / aspect.x));
+        }
 
-        _webViewPrefab = WebViewPrefab.Instantiate(0.5f, 0.5f);
+        _overlay = gameObject.AddComponent<OVROverlay>();
+        _overlay.externalSurfaceWidth = _resolution.x;
+        _overlay.externalSurfaceHeight = _resolution.y;
+        _overlay.transform.localScale = new Vector3(width, width * aspect.y / aspect.x, 1);
+        
+        if (dummyTexture)
+            _overlay.textures = new Texture[] {dummyTexture, dummyTexture};
+        
+#if UNITY_EDITOR
+        _overlay.isExternalSurface = false;
+        _overlay.previewInEditor = true;
+#else
+        _overlay.isExternalSurface = true;
+#endif
+
+        _webViewPrefab = WebViewPrefab.Instantiate(width, width * aspect.y / aspect.x);
+        _webViewPrefab.Visible = false;
         _webViewPrefab.Initialized +=
             (sender, eventArgs) => StartCoroutine(SetupWebView(sender, eventArgs));
-
-        _webViewPrefab.transform.SetParent(transform, false);
-        _webViewPrefab.transform.localEulerAngles = new Vector3(0, 180, 0);
     }
 
     private IEnumerator SetupWebView(object sender, EventArgs eventArgs)
     {
-        Debug.Log("[BONSAI] SetupWebView");
-        _webViewPrefab.Resize(1f, 1f);
-
-        while (_overlay.externalSurfaceObject == IntPtr.Zero || _webViewPrefab.WebView == null)
-        {
-            Debug.Log("[BONSAI] externalSurfaceObject " + _overlay.externalSurfaceObject);
-            Debug.Log("[BONSAI] WebView " + _webViewPrefab.WebView);
-            yield return null;
-        }
-        
-        var surface = _overlay.externalSurfaceObject;
-
-        Debug.Log("[BONSAI] externalSurfaceObject " + _overlay.externalSurfaceObject +
-                  ", WebView " + _webViewPrefab.WebView);
-
-        Debug.Log("[BONSAI] _overlay dimensions "
-                  + _overlay.externalSurfaceWidth + "," + _overlay.externalSurfaceHeight);
+        _webViewPrefab.WebView.LoadUrl(initialURL);
 
 #if UNITY_ANDROID && !UNITY_EDITOR
-        Debug.Log("[BONSAI] SetSurface" + surface);
+        while (_overlay.externalSurfaceObject == IntPtr.Zero || _webViewPrefab.WebView == null)
+        {
+            Debug.Log("[BONSAI] while WebView not setup\nexternalSurfaceObject: <" + _overlay.externalSurfaceObject + ">\nWebView: <" + _webViewPrefab.WebView + ">");
+            yield return null;
+        }
+#endif
+
+        _webViewPrefab.WebView.SetResolution(1);
+        _webViewPrefab.WebView.Resize(_resolution.x, _resolution.y);
+
+        var surface = _overlay.externalSurfaceObject;
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+        Debug.Log("[BONSAI] SetSurface" + surface + ", WebView " + _webViewPrefab.WebView);
         (_webViewPrefab.WebView as AndroidGeckoWebView).SetSurface(surface);
         Debug.Log("[BONSAI] Done SetSurface");
 #endif
-        _webViewPrefab.WebView.LoadUrl("www.google.com");
+        yield break;
+    }
+
+    public static int GetResolution(float width, float distanceEstimate, int pixelPerDegree)
+    {
+        // calculates the optimal resolution along some dimension
+        // x : side of billboard in unity units
+        // distance : estimated closest distance from (user) --- (billboard)
+        // pixelPerDegree : resolving resolution of headset
+        return (int) Math.Round(
+            pixelPerDegree * (360f / (2 * Math.PI)) * 2 * Math.Atan(width / (2 * distanceEstimate))
+        );
     }
 }
---
+++ Assets/BonsaiDesk/Content/Scripts/Editor/AutoBrowserEditor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AutoBrowser))]
+public class AutoBrowserEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+
+        var autoBrowser = target as AutoBrowser;
+        
+        autoBrowser.initialURL = EditorGUILayout.TextField("Initial URL", autoBrowser.initialURL);
+
+        autoBrowser.width = EditorGUILayout.FloatField("Width", autoBrowser.width);
+
+        autoBrowser.aspect = EditorGUILayout.Vector2Field("Aspect Ratio", autoBrowser.aspect);
+        
+        EditorGUILayout.Space();
+        
+        autoBrowser.autoSetResolution = GUILayout.Toggle(autoBrowser.autoSetResolution, 
+            " Automatically Determine Resolution");
+        
+        EditorGUILayout.Space();
+
+        if (!autoBrowser.autoSetResolution)
+        {
+            autoBrowser.xResolution = EditorGUILayout.IntField("X Resolution", autoBrowser.xResolution);
+        }
+        else
+        {
+            EditorGUILayout.LabelField("Calculated X Resolution", 
+                AutoBrowser.GetResolution(
+                  autoBrowser.width, 
+                    autoBrowser.distanceEstimate,
+                    autoBrowser.pixelPerDegree
+                ).ToString()
+            );
+            autoBrowser.distanceEstimate =
+                EditorGUILayout.FloatField("Distance Estimate:", autoBrowser.distanceEstimate);
+            autoBrowser.pixelPerDegree =
+                EditorGUILayout.IntField("Pixels Per Degree", autoBrowser.pixelPerDegree);
+        }
+            
+        EditorGUILayout.Space();
+        
+        autoBrowser.dummyTexture = (Texture) EditorGUILayout.ObjectField("Dummy Texture", autoBrowser.dummyTexture, typeof(Texture), true);
+
+    }
+
+    private void OnValidate()
+    {
+        Repaint();
+    }
+}
