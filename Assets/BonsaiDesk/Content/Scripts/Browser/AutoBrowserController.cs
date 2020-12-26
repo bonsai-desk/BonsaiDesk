76ff0323b45630fb52dde6e2f884916473496511
Cameron Ferguson <s1458162@sms.ed.ac.uk>
2020-12-26 11:50:36 -0500

    +AutoBrowserController

---
+++ Assets/BonsaiDesk/Content/Scripts/Browser/AutoBrowserController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoBrowserController : MonoBehaviour
+{
+    private AutoBrowser _autoBrowser;
+    
+    private void Start()
+    {
+        _autoBrowser = GetComponent<AutoBrowser>();
+        _autoBrowser.BrowserReady += () => { Debug.Log("_autoBrowser.BrowserReady");};
+    }
+}
