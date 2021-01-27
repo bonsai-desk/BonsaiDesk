e795995d669fd8ec05e23fa6fac1a35cdd1de6b9
Cameron Ferguson <cam@hyferg.com>
2021-01-27 10:35:50 -0500

    react handle refresh and reset room code

--- bonsai-react-app/src/DataProvider.jsx
+++ bonsai-react-app/src/DataProvider.jsx
@@ -1,69 +1,124 @@
-import React, {useContext, useEffect, useState} from "react";
-import {action, makeAutoObservable} from "mobx";
+import React, {useContext, useEffect, useState} from 'react';
+import {action, makeAutoObservable} from 'mobx';
+import {observer} from 'mobx-react-lite';
+import axios from 'axios';
 
 export const StoreContext = React.createContext();
 export const useStore = () => useContext(StoreContext);
 
-class Store {
-    ip_address = null;
-    port = null;
-    network_state = null;
+let API_BASE = 'https://api.desk.link';
 
-    constructor () {
-        makeAutoObservable(this)
+class Store {
+  ip_address = null;
+  port = null;
+  network_state = null;
+  loading_room_code = false;
+  _refresh_room_code_handler = null;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  _room_code = null;
+
+  get room_code() {
+    return this._room_code;
+  }
+
+  set room_code(code) {
+    this._room_code = code;
+    if (code) {
+      this._refresh_room_code_handler = setInterval(
+          () => {
+            console.log('refresh ' + this._room_code);
+          }, 1000);
+    } else {
+      clearInterval(this._refresh_room_code_handler);
+      this._refresh_room_code_handler = null;
     }
+  }
+
+  refreshRoomCode() {
+    axios({
+      method: 'post',
+      url: API_BASE + `/rooms/${store.room_code}/refresh`,
+    }).then(response => {
+      console.log('refresh ' + store.room_code);
+    }).catch(err => {
+      console.log(err);
+      this.room_code = null;
+    });
+  }
+
 }
 
-const store = new Store()
-Object.seal(store)
+const store = new Store();
+Object.seal(store);
+let pushStoreList = action((kvList) => {
+  console.log(store);
+  console.log(kvList);
+  kvList.forEach(kv => {
+    store[kv.Key] = kv.Val;
+  });
+  console.log(store);
+});
+let pushStore = action(obj => {
+  for (const prop in obj) {
+    store[prop] = obj[prop];
+  }
+});
 
-let pushStore = action((kvList) => {
-    kvList.forEach(kv => {
-        store[kv.Key] = kv.Val
-    })
-})
+function useListeners() {
+  let [init, setInit] = useState(false);
 
-export const StoreProvider = ({children}) => {
-    let [init, setInit] = useState(false);
+  useEffect(() => {
+    if (init) return;
 
-    useEffect(() => {
-        if (init) return;
+    setInit(true);
 
-        setInit(true)
+    let storeListeners = event => {
+      let json = JSON.parse(event.data);
+      switch (json.Type) {
+        case 'command':
+          switch (json.Message) {
+            case 'pushStore':
+              pushStoreList(json.Data);
+              break;
+            default:
+              console.log('message not handled ' + event.data);
+              break;
+          }
+          break;
+        default:
+          console.log('command not handled ' + event.data);
+          console.log(json);
+          break;
+      }
 
-        let storeListeners = event => {
-            let json = JSON.parse(event.data)
-            switch (json.Type) {
-                case "command":
-                    switch (json.Message) {
-                        case "pushStore":
-                            pushStore(json.Data)
-                            break;
-                        default:
-                            console.log("message not handled " + event.data)
-                            break;
-                    }
-                    break;
-                default:
-                    console.log("command not handled " + event.data)
-                    console.log(json)
-                    break;
-            }
+    };
 
-        }
+    if (window.vuplex != null) {
+      console.log('bonsai: vuplex is not null -> storeListeners');
+      window.vuplex.addEventListener('message', storeListeners);
+    } else {
+      console.log('bonsai: vuplex is null');
+      window.addEventListener('vuplexready', _ => {
+        console.log('bonsai: vuplexready -> storeListeners');
+        window.vuplex.addEventListener('message', storeListeners);
+      });
+    }
 
-        if (window.vuplex != null) {
-            console.log("bonsai: vuplex is not null -> storeListeners")
-            window.vuplex.addEventListener('message', storeListeners)
-        } else {
-            console.log("bonsai: vuplex is null")
-            window.addEventListener('vuplexready', _ => {
-                console.log("bonsai: vuplexready -> storeListeners")
-                window.vuplex.addEventListener('message', storeListeners)
-            })
-        }
+  }, [init]);
+}
 
-    }, [init])
+export const StoreProvider = observer(({children}) => {
 
-    return <StoreContext.Provider value={{store}}>{children}</StoreContext.Provider>;
-};
+  useListeners();
+
+  return <StoreContext.Provider
+      value={{
+        store,
+        pushStore,
+        pushStoreList,
+      }}>{children}</StoreContext.Provider>;
+});
--- bonsai-react-app/src/pages/Menu.jsx
+++ bonsai-react-app/src/pages/Menu.jsx
@@ -1,345 +1,338 @@
-import React, {useEffect, useState} from 'react'
-import "./Menu.css"
-import {postJson} from "../utilities";
-import axios from "axios"
-import DoorOpen from "../static/door-open.svg"
-import {useStore} from "../DataProvider"
-import {BounceLoader} from "react-spinners";
-import {observer} from "mobx-react-lite"
-import {action} from "mobx";
+import React, {useEffect, useState} from 'react';
+import './Menu.css';
+import {postJson} from '../utilities';
+import axios from 'axios';
+import DoorOpen from '../static/door-open.svg';
+import {useStore} from '../DataProvider';
+import {BounceLoader} from 'react-spinners';
+import {observer} from 'mobx-react-lite';
+import {action, autorun} from 'mobx';
 
+let API_BASE = 'https://api.desk.link';
 
-let API_BASE = "https://api.desk.link"
+let buttonClass = 'bg-gray-800 active:bg-gray-700 hover:bg-gray-600 rounded-full p-4 cursor-pointer w-20 h-20 flex flex-wrap content-center';
 
-let buttonClass = "bg-gray-800 active:bg-gray-700 hover:bg-gray-600 rounded-full p-4 cursor-pointer w-20 h-20 flex flex-wrap content-center"
-
-function postJoinRoom (data) {
-    postJson({Type: "command", Message: "joinRoom", data: JSON.stringify(data)})
+function postJoinRoom(data) {
+  postJson({Type: 'command', Message: 'joinRoom', data: JSON.stringify(data)});
 }
 
-function postMouseDown () {
-    postJson({Type: "event", Message: "mouseDown"})
+function postMouseDown() {
+  postJson({Type: 'event', Message: 'mouseDown'});
 }
 
-function postMouseUp () {
-    postJson({Type: "event", Message: "mouseUp"})
+function postMouseUp() {
+  postJson({Type: 'event', Message: 'mouseUp'});
 }
 
-function postHover () {
-    postJson({Type: "event", Message: "hover"})
+function postHover() {
+  postJson({Type: 'event', Message: 'hover'});
 }
 
-function Button (props) {
-    return <div onMouseDown={postMouseDown} onMouseUp={postMouseUp} onMouseEnter={postHover}>{props.children}</div>
+function Button(props) {
+  return <div onMouseDown={postMouseDown} onMouseUp={postMouseUp}
+              onMouseEnter={postHover}>{props.children}</div>;
 }
 
-function ListItem (props) {
-    let {selected, handleClick} = props;
-    let className = selected ?
-        "rounded bg-blue-700 text-white px-3 py-2 cursor-pointer" :
-        "rounded hover:bg-gray-800 active:bg-gray-900 hover:text-white px-3 py-2 cursor-pointer"
-    return (
-        <Button>
-            <div className={className} onClick={handleClick}>
-                {props.children}
-            </div>
-        </Button>
-    )
+function ListItem(props) {
+  let {selected, handleClick} = props;
+  let className = selected ?
+      'rounded bg-blue-700 text-white px-3 py-2 cursor-pointer' :
+      'rounded hover:bg-gray-800 active:bg-gray-900 hover:text-white px-3 py-2 cursor-pointer';
+  return (
+      <Button>
+        <div className={className} onClick={handleClick}>
+          {props.children}
+        </div>
+      </Button>
+  );
 }
 
-function SettingsList (props) {
-    return (
-        <div className={"space-y-1 px-2 h-full overflow-auto"}>
-            {props.children}
-        </div>)
+function SettingsList(props) {
+  return (
+      <div className={'space-y-1 px-2 h-full overflow-auto'}>
+        {props.children}
+      </div>);
 
 }
 
-function SettingsTitle (props) {
-    return <div className={"text-white font-bold text-xl px-5 pt-5 pb-2"}>{props.children}</div>
+function SettingsTitle(props) {
+  return <div
+      className={'text-white font-bold text-xl px-5 pt-5 pb-2'}>{props.children}</div>;
 }
 
-function JoinDeskButton (props) {
-    let {handleClick, char} = props
+function JoinDeskButton(props) {
+  let {handleClick, char} = props;
 
-    return (
-        <Button>
-            <div onClick={() => {
-                handleClick(char)
-            }} className={buttonClass}>
-            <span className={"w-full text-center"}>
+  return (
+      <Button>
+        <div onClick={() => {
+          handleClick(char);
+        }} className={buttonClass}>
+            <span className={'w-full text-center'}>
                 {char}
             </span>
-            </div>
-        </Button>
-    )
-}
-
-function InfoItem (props) {
-    return (
-        <div className={"flex w-full"}>
-            <div className={"flex w-full"}>
-                <div className={"flex flex-wrap content-center  p-2 mr-2"}>
-                    <img className={"h-9"} src={props.imgSrc} alt={""}/>
-                </div>
-                <div>
-                    <div className={"text-xl"}>
-                        {props.title}
-                    </div>
-                    <div className={"text-gray-400"}>
-                        {props.slug}
-                    </div>
-                </div>
-            </div>
-            {props.children}
         </div>
-    )
+      </Button>
+  );
 }
 
-
-function MenuContent (props) {
-    let {name} = props;
-
-    return (
-        <div className={"text-white p-4 h-full pr-8"}>
-            {name ?
-                <div className={"pb-8 text-xl"}>
-                    {name}
-                </div>
-                : ""}
-            <div className={"space-y-8"}>
-                {props.children}
+function InfoItem(props) {
+  return (
+      <div className={'flex w-full'}>
+        <div className={'flex w-full'}>
+          <div className={'flex flex-wrap content-center  p-2 mr-2'}>
+            <img className={'h-9'} src={props.imgSrc} alt={''}/>
+          </div>
+          <div>
+            <div className={'text-xl'}>
+              {props.title}
             </div>
+            <div className={'text-gray-400'}>
+              {props.slug}
+            </div>
+          </div>
         </div>
-    )
-
+        {props.children}
+      </div>
+  );
 }
 
+function MenuContent(props) {
+  let {name} = props;
+
+  return (
+      <div className={'text-white p-4 h-full pr-8'}>
+        {name ?
+            <div className={'pb-8 text-xl'}>
+              {name}
+            </div>
+            : ''}
+        <div className={'space-y-8'}>
+          {props.children}
+        </div>
+      </div>
+  );
+
+}
 
 let HomePage = observer(() => {
-    let {store} = useStore()
 
-    let [roomCode, setRoomCode] = useState("");
+  let {store, pushStore} = useStore();
 
-    let [postedInfo, setPostedInfo] = useState(false);
+  let buttonClass = 'px-2 py-1 bg-gray-800 active:bg-gray-700 hover:bg-gray-600 rounded cursor-pointer flex flex-wrap content-center';
 
-    useEffect(() => {
-        let refreshRoom = () => {
-            if (roomCode) {
-                axios({
-                    method: 'post',
-                    url: API_BASE + `/rooms/${roomCode}/refresh`,
-                }).then(reponse => {
-                    console.log(reponse)
-                }).catch(err => {
-                    console.log(err)
-                    setRoomCode("")
-                    setPostedInfo(false)
-                })
-                console.log("refresh " + roomCode)
+  return (
+      <MenuContent name={'Home'}>
+
+        <InfoItem title={'Desk Code'} slug={'People who have this can join you'}
+                  imgSrc={DoorOpen}>
+          <div className={'text-4xl flex flex-wrap content-center'}>
+            {store.room_code ?
+                <Button>
+                  <div onClick={() => {
+                    pushStore({room_code: null});
+                  }} className={buttonClass}>{store.room_code}</div>
+                </Button>
+
+                : <BounceLoader size={40} color={'#737373'}/>
             }
-        }
+          </div>
+        </InfoItem>
+      </MenuContent>
+  );
+});
 
-        let interval = window.setInterval(refreshRoom, 5000)
+function JoinDeskPage() {
+  let [code, setCode] = useState('');
+  let [loading, setLoading] = useState(false);
+  let [message, setMessage] = useState('');
 
-        return () => {
-            window.clearInterval(interval)
-        }
+  useEffect(() => {
+    if (loading) return;
 
-    }, [roomCode])
-
-    useEffect(() => {
-
-        // remove the room code if no ip/port
-        if (roomCode && !store.ip_address && !store.port) {
-            setRoomCode("");
-            return;
-        }
-
-        // send ip/port out for a room code
-        if (!roomCode && !postedInfo && store.ip_address && store.port) {
-            setPostedInfo(true)
-            let url = API_BASE + "/rooms"
-            axios(
-                {
-                    method: 'post',
-                    url: url,
-                    data: `ip_address=${store.ip_address}&port=${store.port}`,
-                    header: {'content-type': "application/x-www-form-urlencoded"}
-                }
-            ).then(response => {
-                setRoomCode(response.data.tag)
-                setPostedInfo(false)
-            }).catch(err => {
-                setPostedInfo(false)
-            })
-        }
-
-    }, [roomCode, postedInfo, store.ip_address, store.port])
-
-    return (
-        <MenuContent name={"Home"}>
-
-            <InfoItem title={"Desk Code"} slug={"People who have this can join you"} imgSrc={DoorOpen}>
-                <div className={"text-4xl flex flex-wrap content-center"}>
-                    {roomCode ?
-                        roomCode
-                        : <BounceLoader size={40} color={"#737373"}/>
-                    }
-                </div>
-            </InfoItem>
-        </MenuContent>
-    )
-})
-
-function JoinDeskPage () {
-    let [code, setCode] = useState("")
-    let [loading, setLoading] = useState(false);
-    let [message, setMessage] = useState("")
-
-    useEffect(() => {
-        if (loading) return;
-
-        if (code.length === 4) {
-            let url = API_BASE + `/rooms/${code}`
-            console.log(url)
-            axios({
-                method: "get",
-                url: url
-            }).then(response => {
-                postJoinRoom(response.data)
-                setCode("")
-                setLoading(false)
-            }).catch(err => {
-                setCode("")
-                setLoading(false)
-                setMessage("Could not find room, try again")
-            })
-        }
-    }, [loading, code])
-
-    function handleClick (char) {
-        setMessage("")
-        switch (code.length) {
-            case 4:
-                setCode(char)
-                break;
-            default:
-                setCode(code + char)
-                break;
-        }
+    if (code.length === 4) {
+      let url = API_BASE + `/rooms/${code}`;
+      console.log(url);
+      axios({
+        method: 'get',
+        url: url,
+      }).then(response => {
+        postJoinRoom(response.data);
+        setCode('');
+        setLoading(false);
+      }).catch(err => {
+        console.log(err);
+        setCode('');
+        setLoading(false);
+        setMessage('Could not find room, try again');
+      });
     }
+  }, [loading, code]);
 
-    function handleBackspace () {
-        if (code.length > 0) {
-            setCode(code.slice(0, code.length - 1))
-        }
+  function handleClick(char) {
+    setMessage('');
+    switch (code.length) {
+      case 4:
+        setCode(char);
+        break;
+      default:
+        setCode(code + char);
+        break;
     }
+  }
 
-    return (
-        <MenuContent name={"Join Desk"}>
-            <div className={"flex flex-wrap w-full content-center"}>
-                <div className={" w-1/2"}>
-                    <div className={"text-xl"}>
-                        {message}
-                    </div>
-                    <div className={"text-9xl h-full flex flex-wrap content-center justify-center"}>
-                        {code}
-                    </div>
-                </div>
-                <div className={"p-2 rounded space-y-4 text-2xl"}>
-                    <div className={"flex space-x-4"}>
-                        <JoinDeskButton handleClick={handleClick} char={"L"}/>
-                        <JoinDeskButton handleClick={handleClick} char={"R"}/>
-                        <JoinDeskButton handleClick={handleClick} char={"C"}/>
-                    </div>
-                    <div className={"flex space-x-4"}>
-                        <JoinDeskButton handleClick={handleClick} char={"D"}/>
-                        <JoinDeskButton handleClick={handleClick} char={"E"}/>
-                        <JoinDeskButton handleClick={handleClick} char={"F"}/>
-                    </div>
-                    <div className={"flex space-x-4"}>
-                        <JoinDeskButton handleClick={handleClick} char={"G"}/>
-                        <JoinDeskButton handleClick={handleClick} char={"H"}/>
-                        <JoinDeskButton handleClick={handleClick} char={"I"}/>
-                    </div>
-                    <div className={"flex flex-wrap w-full justify-around"}>
-                        <JoinDeskButton handleClick={handleBackspace} char={"<"}/>
-                    </div>
-                </div>
+  function handleBackspace() {
+    if (code.length > 0) {
+      setCode(code.slice(0, code.length - 1));
+    }
+  }
+
+  return (
+      <MenuContent name={'Join Desk'}>
+        <div className={'flex flex-wrap w-full content-center'}>
+          <div className={' w-1/2'}>
+            <div className={'text-xl'}>
+              {message}
             </div>
-        </MenuContent>
-    )
+            <div
+                className={'text-9xl h-full flex flex-wrap content-center justify-center'}>
+              {code}
+            </div>
+          </div>
+          <div className={'p-2 rounded space-y-4 text-2xl'}>
+            <div className={'flex space-x-4'}>
+              <JoinDeskButton handleClick={handleClick} char={'L'}/>
+              <JoinDeskButton handleClick={handleClick} char={'R'}/>
+              <JoinDeskButton handleClick={handleClick} char={'C'}/>
+            </div>
+            <div className={'flex space-x-4'}>
+              <JoinDeskButton handleClick={handleClick} char={'D'}/>
+              <JoinDeskButton handleClick={handleClick} char={'E'}/>
+              <JoinDeskButton handleClick={handleClick} char={'F'}/>
+            </div>
+            <div className={'flex space-x-4'}>
+              <JoinDeskButton handleClick={handleClick} char={'G'}/>
+              <JoinDeskButton handleClick={handleClick} char={'H'}/>
+              <JoinDeskButton handleClick={handleClick} char={'I'}/>
+            </div>
+            <div className={'flex flex-wrap w-full justify-around'}>
+              <JoinDeskButton handleClick={handleBackspace} char={'<'}/>
+            </div>
+          </div>
+        </div>
+      </MenuContent>
+  );
 }
 
-function ContactsPage () {
-    return <MenuContent name={"Contacts"}>
-    </MenuContent>
+function ContactsPage() {
+  return <MenuContent name={'Contacts'}>
+  </MenuContent>;
 }
 
-let SettingsPage = observer((props) => {
-    let {store} = useStore();
+let SettingsPage = observer(() => {
+  let {store} = useStore();
 
-    let addFakeIpPort = action((store) => {
-        store.ip_address = 1234
-        store.port = 4321
-    })
+  let addFakeIpPort = action((store) => {
+    store.ip_address = 1234;
+    store.port = 4321;
+  });
 
-    let rmFakeIpPort = action(store => {
-        store.ip_address = null
-        store.port = null
-    })
+  let rmFakeIpPort = action(store => {
+    store.ip_address = null;
+    store.port = null;
+  });
 
-    return (
-        <MenuContent name={"Settings"}>
-            <div className={buttonClass} onClick={() => {
-                addFakeIpPort(store)
-            }}>+fake ip/port
-            </div>
-            <div className={buttonClass} onClick={() => {
-                rmFakeIpPort(store)
-            }}>-fake ip/port
-            </div>
-            <ul>
-                {Object.entries(store).map(info => {
-                    return <li key={info[0]}>{info[0]}{": "}{info[1]}</li>
-                })}
-            </ul>
-        </MenuContent>
-    )
-})
+  return (
+      <MenuContent name={'Settings'}>
+        <div className={buttonClass} onClick={() => {
+          addFakeIpPort(store);
+        }}>+fake ip/port
+        </div>
+        <div className={buttonClass} onClick={() => {
+          rmFakeIpPort(store);
+        }}>-fake ip/port
+        </div>
+        <ul>
+          {Object.entries(store).map(info => {
+            return <li key={info[0]}>{info[0]}{': '}{info[1]}</li>;
+          })}
+        </ul>
+      </MenuContent>
+  );
+});
 
 const pages = [
-    {name: "Home", component: HomePage},
-    {name: "Join Desk", component: JoinDeskPage},
-    {name: "Contacts", component: ContactsPage},
-    {name: "Settings", component: SettingsPage},
-]
+  {name: 'Home', component: HomePage},
+  {name: 'Join Desk', component: JoinDeskPage},
+  {name: 'Contacts', component: ContactsPage},
+  {name: 'Settings', component: SettingsPage},
+];
 
 let Menu = () => {
 
-    let [active, setActive] = useState(0)
+  let {store, pushStore} = useStore();
 
-    let SelectedPage = pages[active].component;
+  let [active, setActive] = useState(0);
 
-    return (
-        <div className={"flex text-lg text-gray-500 h-full"}>
-            <div className={"w-4/12 bg-black h-full overflow-hidden"}>
-                <SettingsTitle>
-                    Menu
-                </SettingsTitle>
-                <SettingsList>
-                    {pages.map((info, i) => {
-                        return <ListItem key={info.name} handleClick={() => {
-                            setActive(i)
-                        }} selected={active === i}>{info.name}</ListItem>
-                    })}
-                </SettingsList>
-            </div>
-            <div className={"w-full"}>
-                <SelectedPage/>
-            </div>
+  let SelectedPage = pages[active].component;
+
+  const disposer = autorun(() => {
+    // remove room code if
+    if (store.room_code && (!store.ip_address || !store.port)) {
+      console.log('rm room code');
+      pushStore({room_code: null});
+      return;
+    }
+
+    // send ip/port out for a room code
+    if (!store.room_code && !store.loading_room_code && store.ip_address &&
+        store.port) {
+      console.log('fetch room code');
+      pushStore({loading_room_code: true});
+      let url = API_BASE + '/rooms';
+      axios(
+          {
+            method: 'post',
+            url: url,
+            data: `ip_address=${store.ip_address}&port=${store.port}`,
+            header: {'content-type': 'application/x-www-form-urlencoded'},
+          },
+      ).then(response => {
+        pushStore({room_code: response.data.tag, loading_room_code: false});
+      }).catch(err => {
+        console.log(err);
+        pushStore({loading_room_code: false});
+      });
+    }
+  });
+
+  useEffect(() => {
+    return () => {
+      console.log('disposing menu');
+      pushStore({room_code: null});
+      disposer();
+    };
+  }, [disposer, pushStore]);
+
+  return (
+      <div className={'flex text-lg text-gray-500 h-full'}>
+        <div className={'w-4/12 bg-black h-full overflow-hidden'}>
+          <SettingsTitle>
+            Menu
+          </SettingsTitle>
+          <SettingsList>
+            {pages.map((info, i) => {
+              return <ListItem key={info.name} handleClick={() => {
+                setActive(i);
+              }} selected={active === i}>{info.name}</ListItem>;
+            })}
+          </SettingsList>
         </div>
-    )
-}
+        <div className={'w-full'}>
+          <SelectedPage/>
+        </div>
+      </div>
+  );
+};
+
 export default Menu;
