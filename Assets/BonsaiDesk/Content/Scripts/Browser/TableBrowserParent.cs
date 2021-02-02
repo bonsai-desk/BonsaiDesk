cd00a4f8e50f34530cf96a44ba9a704f5f959042
Cameron Ferguson <cam@hyferg.com>
2021-02-02 11:44:11 -0500

    toggle browser position

---
+++ Assets/BonsaiDesk/Content/Scripts/Browser/TableBrowserParent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableBrowserParent : MonoBehaviour {
+
+    public bool flat = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TogglePosition() {
+        if (flat) {
+            transform.localPosition = new Vector3(0f, 0.078f, 0.221f);
+            transform.localEulerAngles = new Vector3(-61.7f, 0f, 0f);
+        }
+        else {
+            transform.localPosition = new Vector3(0f, 0f, 0f);
+            transform.localEulerAngles = new Vector3(0f, 0f, 0f);
+        }
+
+        flat = !flat;
+    }
+    
+    
+}
