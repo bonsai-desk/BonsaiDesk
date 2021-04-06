02d4adb7f6aaeb507f7658967dbe4c219f41b548
Callum <Calxferg@gmail.com>
2021-04-06 18:50:59 -0400

    fix network hands

--- Assets/BonsaiDesk/Content/Scripts/Network/NetworkHand.cs
+++ Assets/BonsaiDesk/Content/Scripts/Network/NetworkHand.cs
@@ -74,7 +74,7 @@ public class NetworkHand : NetworkBehaviour
         else
             physicsHandPrefab = Resources.Load<GameObject>("Right_Hand");
         var hand = Instantiate(physicsHandPrefab);
-        hand.transform.GetChild(1).GetComponent<PhysicsHandController>().SetKinematic();
+        hand.transform.GetChild(0).GetComponent<PhysicsHandController>().SetKinematic();
         var physicsMapper = hand.transform.GetChild(1).GetComponent<OVRHandTransformMapper>();
         physicsMapper.targetObject = transform;
         physicsMapper.capsulesParent = transform;
