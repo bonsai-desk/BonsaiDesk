403ff10fcce9d81bc86ab2b5a05db622508ddff9
Cameron Ferguson <cam@hyferg.com>
2021-04-22 13:05:32 -0400

    hard fade out cubemap when menu ready

--- Assets/BonsaiDesk/Content/Scripts/Launch/LogoMove.cs
+++ Assets/BonsaiDesk/Content/Scripts/Launch/LogoMove.cs
@@ -1,28 +1,17 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LogoMove : MonoBehaviour
 {
+    public OVROverlay overlay;
     private void Awake()
     {
-        //DontDestroyOnLoad(gameObject);
-        //SceneManager.sceneLoaded += SceneLoad;
+        DontDestroyOnLoad(gameObject);
     }
 
-    void SceneLoad(Scene scene, LoadSceneMode mode)
+    public void FadeOut()
     {
-        return;
-        if (mode == LoadSceneMode.Single && SceneManager.GetActiveScene().buildIndex == 1)
-        {
-            var centerEyeAnchor = GameObject.Find("CenterEyeAnchor");
-
-            // transform.position = centerEyeAnchor.transform.TransformPoint(LaunchManager.LogoLocalPosition);
-            transform.position = centerEyeAnchor.transform.position +
-                                 new Vector3(-LaunchManager.LogoLocalPosition.x, LaunchManager.LogoLocalPosition.y, -LaunchManager.LogoLocalPosition.z);
-            // transform.rotation = centerEyeAnchor.transform.rotation * LaunchManager.LogoLocalRotation;
-        }
+        Debug.Log("Fade Out");
+        overlay.enabled = false;
     }
 }
