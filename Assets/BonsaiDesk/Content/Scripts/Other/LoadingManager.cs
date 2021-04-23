e7c737033355785e1ac6ffa4a74e10831ae1f793
Cameron Ferguson <cam@hyferg.com>
2021-04-23 19:03:35 -0400

    90hz for quest 2

--- Assets/BonsaiDesk/Content/Scripts/Other/LoadingManager.cs
+++ Assets/BonsaiDesk/Content/Scripts/Other/LoadingManager.cs
@@ -1,30 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class LoadingManager : MonoBehaviour
 {
     public TableBrowser browser;
-    private float _startedAt;
     private bool _initialFade;
-    
-    void Start()
+    private bool _setFreq;
+    private float _startedAt;
+
+    private void Start()
     {
         _startedAt = Time.realtimeSinceStartup;
         BonsaiLog($"Started at {_startedAt}");
         browser.ListenersReady += HandleListenersReady;
     }
 
-    private void HandleListenersReady()
+    private void Update()
     {
-        
-        BonsaiLog($"Fade in after {Time.realtimeSinceStartup - _startedAt} seconds");
-        _initialFade = true;
-        FadeIn();
-    }
+        if (!_setFreq && OVRManager.OVRManagerinitialized)
+        {
+            _setFreq = true;
 
-    void Update()
-    {
+            float newFreq = 72f;
+            foreach (var freq in OVRManager.display.displayFrequenciesAvailable)
+            {
+                if (freq <= 90f)
+                {
+                    newFreq = freq;
+                }
+            }
+
+            BonsaiLog($"Setting frequency to {newFreq}");
+            OVRPlugin.systemDisplayFrequency = newFreq;
+        }
+
         if (!_initialFade && Time.realtimeSinceStartup - _startedAt > 5f)
         {
             BonsaiLogWarning($"Failed to trigger fade in after 5 seconds, triggering now ({Time.realtimeSinceStartup})");
@@ -32,6 +40,14 @@ public class LoadingManager : MonoBehaviour
             FadeIn();
         }
     }
+
+    private void HandleListenersReady()
+    {
+        BonsaiLog($"Fade in after {Time.realtimeSinceStartup - _startedAt} seconds");
+        _initialFade = true;
+        FadeIn();
+    }
+
     private void BonsaiLog(string msg)
     {
         Debug.Log("<color=orange>BonsaiLoad: </color>: " + msg);
