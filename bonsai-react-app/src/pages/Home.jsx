42fd33004ad4d94b36ba2a29271a186405a40c35
Cameron Ferguson <cam@hyferg.com>
2021-04-30 15:11:29 -0400

    re-arrange imports

--- bonsai-react-app/src/pages/Home.jsx
+++ bonsai-react-app/src/pages/Home.jsx
@@ -1,148 +1,149 @@
-import {observer} from 'mobx-react-lite';
-import {NetworkManagerMode, useStore} from '../DataProvider';
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import {apiBase} from '../utilities';
-import {postCloseRoom, postJoinRoom, postKickConnectionId, postLeaveRoom, postOpenRoom} from '../api';
-import {InfoItem} from '../components/InfoItem';
-import DoorOpen from '../static/door-open.svg';
-import {Button} from '../components/Button';
-import {grayButtonClassInert, greenButtonClass, redButtonClass, roundButtonClass} from '../cssClasses';
-import LinkImg from '../static/link.svg';
+import {observer} from 'mobx-react-lite';
 import {BeatLoader, BounceLoader} from 'react-spinners';
-import ThinkingFace from '../static/thinking-face.svg';
-import {MenuContent} from '../components/MenuContent';
 import {useHistory} from 'react-router-dom';
 
+import DoorOpen from '../static/door-open.svg';
+import LinkImg from '../static/link.svg';
+import ThinkingFace from '../static/thinking-face.svg';
+import {grayButtonClassInert, greenButtonClass, redButtonClass, roundButtonClass} from '../cssClasses';
+import {InfoItem} from '../components/InfoItem';
+import {Button} from '../components/Button';
+import {MenuContent} from '../components/MenuContent';
+import {apiBase} from '../utilities';
+import {postCloseRoom, postJoinRoom, postKickConnectionId, postLeaveRoom, postOpenRoom} from '../api';
+import {NetworkManagerMode, useStore} from '../DataProvider';
+
 function ConnectedClient(props) {
-  let {info} = props;
-  let {Name, ConnectionId} = info;
+    let {info} = props;
+    let {Name, ConnectionId} = info;
 
-  const hostClass = 'bg-gray-800 rounded-full p-4 h-20 flex flex-wrap content-center';
-  const clientClass = 'bg-gray-800 active:bg-red-700 hover:bg-red-600 rounded-full p-4 cursor-pointer h-20 flex flex-wrap content-center';
+    const hostClass = 'bg-gray-800 rounded-full p-4 h-20 flex flex-wrap content-center';
+    const clientClass = 'bg-gray-800 active:bg-red-700 hover:bg-red-600 rounded-full p-4 cursor-pointer h-20 flex flex-wrap content-center';
 
-  if (ConnectionId === 0) {
-    return (
-        <div className={hostClass}>
-          <div
-              className={'flex content-center p-2 space-x-4'}>
-            <div>
-              <img className={'h-9 w-9'} src={ThinkingFace} alt={''}/>
-            </div>
-            <div>
-              {Name}
-            </div>
-          </div>
-        </div>
-    );
-  } else {
-    return (
-        <Button className={clientClass} handleClick={() => {
-          postKickConnectionId(ConnectionId);
-        }}>
-          <div
-              className={'flex content-center p-2 space-x-4'}>
-            <div>
-              <img className={'h-9 w-9'} src={ThinkingFace} alt={''}/>
-            </div>
-            <div>
-              {Name}
-            </div>
-          </div>
-        </Button>
-    );
+    if (ConnectionId === 0) {
+        return (
+                <div className={hostClass}>
+                    <div
+                            className={'flex content-center p-2 space-x-4'}>
+                        <div>
+                            <img className={'h-9 w-9'} src={ThinkingFace} alt={''}/>
+                        </div>
+                        <div>
+                            {Name}
+                        </div>
+                    </div>
+                </div>
+        );
+    } else {
+        return (
+                <Button className={clientClass} handleClick={() => {
+                    postKickConnectionId(ConnectionId);
+                }}>
+                    <div
+                            className={'flex content-center p-2 space-x-4'}>
+                        <div>
+                            <img className={'h-9 w-9'} src={ThinkingFace} alt={''}/>
+                        </div>
+                        <div>
+                            {Name}
+                        </div>
+                    </div>
+                </Button>
+        );
 
-  }
+    }
 
 }
 
 function OpenRoomItem() {
-  return <InfoItem title={'Room'} slug={'Invite others'} imgSrc={DoorOpen}>
-    <Button className={greenButtonClass} handleClick={postOpenRoom}>
-      Open Up
-    </Button>
-  </InfoItem>;
+    return <InfoItem title={'Room'} slug={'Invite others'} imgSrc={DoorOpen}>
+        <Button className={greenButtonClass} handleClick={postOpenRoom}>
+            Open Up
+        </Button>
+    </InfoItem>;
 }
 
 const CloseRoomItem = observer(() => {
-  let {store} = useStore();
+    let {store} = useStore();
 
-  let handleCloseRoom = () => {
-    if (store.RoomCode) {
-      axios({
-        method: 'delete',
-        url: apiBase(store) + '/rooms/' + store.RoomCode,
-      }).then(r => {
-        if (r.status === 200) {
-          console.log(`deleted room ${store.RoomCode}`);
+    let handleCloseRoom = () => {
+        if (store.RoomCode) {
+            axios({
+                method: 'delete',
+                url: apiBase(store) + '/rooms/' + store.RoomCode,
+            }).then(r => {
+                if (r.status === 200) {
+                    console.log(`deleted room ${store.RoomCode}`);
+                }
+            }).catch(console.log);
         }
-      }).catch(console.log);
-    }
-    postCloseRoom();
-  };
+        postCloseRoom();
+    };
 
-  return <InfoItem title={'Room'} slug={'Ready to accept connections'}
-                   imgSrc={DoorOpen}>
-    <Button className={redButtonClass} handleClick={handleCloseRoom}>
-      Close
-    </Button>
-  </InfoItem>;
+    return <InfoItem title={'Room'} slug={'Ready to accept connections'}
+                     imgSrc={DoorOpen}>
+        <Button className={redButtonClass} handleClick={handleCloseRoom}>
+            Close
+        </Button>
+    </InfoItem>;
 });
 const DeskCodeItem = observer(() => {
-  let {store} = useStore();
-  const roomCodeCLass = 'text-5xl ';
-  return <InfoItem title={'Desk Code'}
-                   slug={'People who have this can join you'}
-                   imgSrc={LinkImg}>
-    <div className={'h-20 flex flex-wrap content-center'}>
-      {store.RoomCode ?
-          <div className={roomCodeCLass}>{store.RoomCode}</div>
+    let {store} = useStore();
+    const roomCodeCLass = 'text-5xl ';
+    return <InfoItem title={'Desk Code'}
+                     slug={'People who have this can join you'}
+                     imgSrc={LinkImg}>
+        <div className={'h-20 flex flex-wrap content-center'}>
+            {store.RoomCode ?
+                    <div className={roomCodeCLass}>{store.RoomCode}</div>
 
-          :
-          <div className={grayButtonClassInert}><BeatLoader size={8}
-                                                            color={'#737373'}/>
-          </div>
-      }
-    </div>
-  </InfoItem>;
+                    :
+                    <div className={grayButtonClassInert}><BeatLoader size={8}
+                                                                      color={'#737373'}/>
+                    </div>
+            }
+        </div>
+    </InfoItem>;
 });
 const RoomInfo = observer(() => {
-  let {store} = useStore();
+    let {store} = useStore();
 
-  if (store.NetworkInfo.RoomOpen) {
-    return (
-        <React.Fragment>
-          <CloseRoomItem/>
-          <DeskCodeItem/>
-        </React.Fragment>
-    );
-  } else {
-    return (
-        <React.Fragment>
-          <OpenRoomItem/>
-        </React.Fragment>
-    );
-  }
+    if (store.NetworkInfo.RoomOpen) {
+        return (
+                <React.Fragment>
+                    <CloseRoomItem/>
+                    <DeskCodeItem/>
+                </React.Fragment>
+        );
+    } else {
+        return (
+                <React.Fragment>
+                    <OpenRoomItem/>
+                </React.Fragment>
+        );
+    }
 
 });
 export const HostHomePage = observer(() => {
 
-  let {store} = useStore();
+    let {store} = useStore();
 
-  return (
-      <React.Fragment>
-        <RoomInfo/>
-        {store.PlayerInfos.length > 0 && store.NetworkInfo.RoomOpen ?
+    return (
             <React.Fragment>
-              <div className={'text-xl'}>People in Your Room</div>
-              <div className={'flex space-x-2'}>
-                {store.PlayerInfos.map(info => <ConnectedClient info={info}/>)}
-              </div>
+                <RoomInfo/>
+                {store.PlayerInfos.length > 0 && store.NetworkInfo.RoomOpen ?
+                        <React.Fragment>
+                            <div className={'text-xl'}>People in Your Room</div>
+                            <div className={'flex space-x-2'}>
+                                {store.PlayerInfos.map(info => <ConnectedClient info={info}/>)}
+                            </div>
+                        </React.Fragment>
+                        :
+                        ''}
             </React.Fragment>
-            :
-            ''}
-      </React.Fragment>
-  );
+    );
 
 });
 
