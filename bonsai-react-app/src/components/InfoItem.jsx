dcef81fd7a060bed54d5a81692f16fe66e02932f
Cameron Ferguson <cam@hyferg.com>
2021-05-13 21:03:51 -0400

    v0.1.4b46

--- bonsai-react-app/src/components/InfoItem.jsx
+++ bonsai-react-app/src/components/InfoItem.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
 
-export function InfoItem({imgSrc, title, slug, children}) {
-  return (
-      <div className={'flex w-full justify-between pr-4'}>
-        <div className={'flex w-auto'}>
-          <div className={'flex flex-wrap content-center p-2 mr-2'}>
-            {imgSrc ?
-                <img className={'h-9 w-9'} src={imgSrc} alt={''}/>
-                : ''
-            }
-          </div>
-          <div className={'my-auto'}>
-            <div className={'text-xl'}>
-              {title}
+export function InfoItem({imgSrc, title, slug, children, rightPad = true}) {
+    let className = 'flex w-full justify-between pr-4';
+    if (!rightPad) {
+        className = 'flex w-full justify-between';
+    }
+   
+    return (
+            <div className={className}>
+                <div className={'flex w-auto'}>
+                    <div className={'flex flex-wrap content-center p-2 mr-2'}>
+                        {imgSrc ?
+                                <img className={'h-9 w-9'} src={imgSrc} alt={''}/>
+                                : ''
+                        }
+                    </div>
+                    <div className={'my-auto'}>
+                        <div className={'text-xl'}>
+                            {title}
+                        </div>
+                        <div className={'text-gray-400'}>
+                            {slug}
+                        </div>
+                    </div>
+                </div>
+                {children}
             </div>
-            <div className={'text-gray-400'}>
-              {slug}
-            </div>
-          </div>
-        </div>
-        {children}
-      </div>
-  );
+    );
 }
