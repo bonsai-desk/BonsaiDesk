67450050c6a60159860695511f7edb7f7933df67
Callum <Calxferg@gmail.com>
2021-05-22 20:27:22 -0400

    remove save default

--- Assets/BonsaiDesk/Content/Scripts/Hands/HandTicks/BlockBreakHand.cs
+++ Assets/BonsaiDesk/Content/Scripts/Hands/HandTicks/BlockBreakHand.cs
@@ -46,10 +46,10 @@ public class BlockBreakHand : MonoBehaviour, IHandTick
         _particleSystem = _particleObject.GetComponent<ParticleSystem>();
         _mainModule = _particleSystem.main;
         SetBreakMode(BreakMode.None);
-        if (playerHand.skeletonType == OVRSkeleton.SkeletonType.HandRight)
-        {
-            SetBreakMode(BreakMode.Save);
-        }
+        // if (playerHand.skeletonType == OVRSkeleton.SkeletonType.HandRight)
+        // {
+        //     SetBreakMode(BreakMode.Save);
+        // }
     }
 
     public void SetBreakMode(BreakMode breakMode)
