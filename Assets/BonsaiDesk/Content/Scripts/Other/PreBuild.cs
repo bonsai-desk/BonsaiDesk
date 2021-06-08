357d8c7d04dcbb78fbf488ceb8f648913b5a4186
Callum <Calxferg@gmail.com>
2021-06-08 16:38:41 -0400

    copy android passwords prebuild script

--- Assets/BonsaiDesk/Content/Scripts/Other/PreBuild.cs
+++ Assets/BonsaiDesk/Content/Scripts/Other/PreBuild.cs
@@ -1,4 +1,5 @@
 ﻿#if UNITY_EDITOR
+using System;
 using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
@@ -20,8 +21,31 @@ public class PreBuild : IPreprocessBuildWithReport
             Debug.LogError("Cant find NetworkManagerGame for build");
         }
 
-        PlayerSettings.keystorePass = "";
-        PlayerSettings.keyaliasPass = "";
+        try
+        {
+            var strDir = System.IO.Directory.GetCurrentDirectory();
+            var passPath = System.IO.Path.Combine(strDir, "android_pass.txt");
+
+            if (!System.IO.File.Exists(passPath))
+            {
+                System.IO.File.Create(passPath);
+            }
+
+            var lines = System.IO.File.ReadAllLines(passPath);
+            if (lines.Length == 2)
+            {
+                PlayerSettings.keystorePass = lines[0];
+                PlayerSettings.keyaliasPass = lines[1];
+            }
+            else
+            {
+                Debug.LogError("android_pass.txt should have exactly 2 lines.");
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Get android_pass.txt error: " + e);
+        }
     }
 }
 #endif
