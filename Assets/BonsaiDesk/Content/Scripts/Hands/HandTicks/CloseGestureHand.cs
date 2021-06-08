70b869b4585078135d52a24e1e0c7e6169c9a356
Callum <Calxferg@gmail.com>
2021-06-07 22:43:07 -0400

    fist gesture progress

--- Assets/BonsaiDesk/Content/Scripts/Hands/HandTicks/CloseGestureHand.cs
+++ Assets/BonsaiDesk/Content/Scripts/Hands/HandTicks/CloseGestureHand.cs
@@ -2,22 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class CloseGestureHand : MonoBehaviour, IHandTick
 {
     public PlayerHand playerHand { get; set; }
 
     public AngleToObject angleToHead;
-    public Transform head;
     public UnityEvent action;
 
-    private const float AngleToHeadThreshold = 40f;
+    private bool _gestureInProgress;
+    private float _progress;
+    private bool _activated;
+    private const float ActivationTime = 0.35f;
+
+    public Image progressImage;
+    public GameObject fistProgress;
+    public GameObject fistDescription;
 
     public void Tick()
     {
         if (playerHand.GetGestureStart(PlayerHand.Gesture.Fist) && angleToHead.AngleBelowThreshold() && !playerHand.stylus.gameObject.activeInHierarchy)
-            // Vector3.Angle(-angleToHead.transform.forward, head.forward) < AngleToHeadThreshold)
         {
+            _gestureInProgress = true;
+        }
+        if (playerHand.GetGesture(PlayerHand.Gesture.Fist) && angleToHead.AngleBelowThreshold() && !playerHand.stylus.gameObject.activeInHierarchy)
+        {
+            fistDescription.SetActive(false);
+            if (_gestureInProgress)
+            {
+                fistProgress.SetActive(true);
+            
+                if (progressImage)
+                {
+                    _progress += Time.deltaTime * (1f / ActivationTime);
+                }
+                else
+                {
+                    _progress = 1f;
+                }
+            }
+        }
+        else
+        {
+            fistProgress.SetActive(false);
+            fistDescription.SetActive(true);
+            _progress = 0;
+            _activated = false;
+            _gestureInProgress = false;
+        }
+
+        if (progressImage)
+        {
+            progressImage.fillAmount = Mathf.Clamp01(_progress);
+        }
+
+        if (!_activated && _progress >= 1f)
+        {
+            _activated = true;
             action?.Invoke();
         }
     }
